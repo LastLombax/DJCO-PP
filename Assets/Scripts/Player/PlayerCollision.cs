f91f9f712cec069465f3b1dd3201e472dd83fcd2
LastLombax <up201503447@fe.up.pt>
2019-02-21 17:25:52 +0000

    Started collision and added asset from store(pls don't reject it)

---
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+public class PlayerCollision : MonoBehaviour
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Collision!!!");
+            GetComponent<PlayerStats>().EnemyCollision();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,30 @@
+﻿using Kryz.CharacterStats;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public CharacterStat health;
+    public CharacterStat energy;
+    public CharacterStat speed;
+
+    void Start()
+    {
+        health.BaseValue = 5;
+        energy.BaseValue = 5;
+        speed.BaseValue = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //StatModifier mod = new StatModifier(10, StatModType.Flat);
+        //health.AddModifier(mod);
+        //Debug.Log(health.Value);
+    }
+
+    public void EnemyCollision()
+    {
+        StatModifier collisionDmg = new StatModifier(-10, StatModType.Flat);
+        health.AddModifier(collisionDmg);
+    }
+}
