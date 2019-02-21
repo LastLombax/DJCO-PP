ecb563dfee2d11069757f02db93ff8306ffdf480
titarta <tiagorvcarvalho@gmail.com>
2019-02-21 16:30:36 +0000

    Missing files from previous commit
    i dunno how this happened

--- Assets/Scripts/PlayerMobility.cs
+++ Assets/Scripts/PlayerMobility.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlayerMobility : MonoBehaviour
 {
     public float speed;
+    public Animator anim;
 
     void Update()
     {
@@ -16,9 +17,27 @@ public class PlayerMobility : MonoBehaviour
         var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
 
-        transform.rotation = rot;
+        var ang = rot.eulerAngles.z;
+        //Debug.Log(rot.eulerAngles.z);
+        if ((ang >= 0 && ang <= 45) || (ang <= 360 && ang >= 315))
+        {
+            anim.Play("PlayerAnimationUp");
+        }
+        if (ang > 45 && ang < 135)
+        {
+            anim.Play("PlayerAnimationLeft");
+        }
+        if (ang > 135 && ang < 225)
+        {
+            anim.Play("PlayerAnimationDown");
+        }
+        if (ang > 225 && ang < 315)
+        {
+            anim.Play("PlayerAnimationRight");
+        }
+        //transform.rotation = rot;
 
-        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        //transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
     }
 
     private void PlayerMovement()
