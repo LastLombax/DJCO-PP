cb3c6c20f0dcfa09eb01a7de22d3bf5019660f2e
LastLombax <up201503447@fe.up.pt>
2019-02-25 20:35:54 +0000

    Added Swarmer

---
+++ Assets/Scripts/Enemies/Swarmer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swarmer : Enemy
+{
+    public GameObject spawnedEnemy;
+
+    private float healthValue = 5f;
+    private float speedValue = 0f;
+    private float nextSpawn = 0;
+    private float spawnRate = 3;
+
+    //private int maxChildren = 4;
+
+    void Start() {
+        nextSpawn = Time.time + spawnRate;
+        player = GameObject.Find("Player");
+        setStats(healthValue,speedValue);
+    }
+
+    void Update() {
+        EnemyRotation();
+        EnemyMovement();
+        if (Time.time > nextSpawn) {
+            nextSpawn = Time.time + spawnRate;
+            spawn();
+        }
+    }
+
+    private void spawn()
+    {
+        var projectilePos = transform.position;
+        var shot = Instantiate(spawnedEnemy, projectilePos, Quaternion.identity);
+        Physics2D.IgnoreCollision(shot.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+    }
+}
