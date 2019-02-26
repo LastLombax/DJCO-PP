480cb960341634c6fd83e3a9c4b7d1fa9c24bda7
LastLombax <up201503447@fe.up.pt>
2019-02-26 17:23:19 +0000

    Problems in Weapon rotation

--- Assets/Scripts/Player/PlayerSkills.cs
+++ Assets/Scripts/Player/PlayerSkills.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerSkills : MonoBehaviour
 {
-
+    public float weaponDistance = 1.5f;
     public GameObject projectile;
+    public GameObject weapon;
     Vector2 projectilePos;
     public float fireRate = 0.05f;
     float nextFire = 0;
@@ -34,6 +35,11 @@ public class PlayerSkills : MonoBehaviour
             nextFire = Time.time + fireRate;
             fireball();
         }
+
+        if (Input.GetMouseButtonDown(0) && GameObject.Find("Weapon(Clone)") == null) {
+            attack();
+        }
+
     }
 
     private void fireball()
@@ -42,5 +48,21 @@ public class PlayerSkills : MonoBehaviour
         var fireBall = Instantiate(projectile, projectilePos, Quaternion.identity);
 
         Physics2D.IgnoreCollision(fireBall.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+    }
+    private void attack(){
+       
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePosition - transform.position;
+
+        direction = new Vector3(direction.x, direction.y, 0);
+
+        var dirLength = direction.normalized;
+     
+        var weaponPos = transform.position + dirLength*weaponDistance;
+
+        var attack = Instantiate(weapon, weaponPos, Quaternion.identity);
+
+        Physics2D.IgnoreCollision(attack.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+
     }
 }
