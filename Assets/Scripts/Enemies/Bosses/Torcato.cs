938bb8480541f4208795e3418a8a52fe6c3e8c02
JoseFreixo <josefreixo15@gmail.com>
2019-03-10 19:23:23 +0000

    Logic of MDIS complete, needs the worlds and rules

---
+++ Assets/Scripts/Enemies/Bosses/Torcato.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torcato : MonoBehaviour
+{
+    public GameManager gm;
+
+    public Sprite[] worlds;
+    public Sprite[] rules;
+
+    private bool[] answers = {false, true, true, false, true};
+    private int level;
+    private bool state;
+    private bool over;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        level = 0;
+        state = true;
+        over = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (level >= answers.Length) {
+            over = true;
+        }
+
+        if (over) {
+            if (state) {
+                Debug.Log("Passaste");
+            } else {
+                Debug.Log("Lamento, mas não obviteste uma classificação satisfatória...");
+            }
+            StartCoroutine(gm.Load2Scene("MainRoom"));
+        }
+    }
+
+    public void CheckAnswer(bool answer) 
+    {
+        Debug.Log("Here");
+        state = state && answer;
+        level++;
+    }
+}
---
+++ Assets/Scripts/LogicMDIS/Clickable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Clickable : MonoBehaviour
+{
+    private Torcato torcato;
+    public abstract bool check { get; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        torcato = GameObject.Find("Torcato").GetComponent<Torcato>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        torcato.CheckAnswer(check);
+    }
+}
