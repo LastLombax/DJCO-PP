23135b2385121498516e86248158e3eeb7843bf5
titarta <tiagorvcarvalho@gmail.com>
2019-03-17 10:31:19 +0000

    sent sprite and script for hiddenDoor

---
+++ Assets/Scripts/Level/WallDoorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDoorScript : MonoBehaviour
+{
+    public GameObject wireOff;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(wireOff.GetComponent<WireScript>().IsActive())
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "DoorWallHidden";
+        } else
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "Walls";
+        }
+    }
+}
