25e3ef8ec07aff8357e788337f0d64b713fd9520
JoseFreixo <josefreixo15@gmail.com>
2019-03-19 00:09:46 +0000

    Rough Start menu added, can't figure out positions

---
+++ Assets/Scripts/StartButtons.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButtons : MonoBehaviour
+{
+    public GameObject Buttons;
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+}
