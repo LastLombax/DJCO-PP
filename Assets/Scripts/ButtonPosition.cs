8a8f85ea928a9799b8e2e8abb76ee36a81001153
JoseFreixo <josefreixo15@gmail.com>
2019-03-19 17:18:38 +0000

    Deez F'ing buttons

---
+++ Assets/Scripts/ButtonPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPosition : MonoBehaviour
+{
+    public float xPos;
+    public float yPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 positionCam = Camera.main.ViewportToWorldPoint(new Vector3(xPos, yPos, 0));
+        transform.position = new Vector3(positionCam.x, positionCam.y, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
